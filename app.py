c4eee8530b24271858a76bcf05b714ba4c72ae14
Nathaniel Murphy <njmurph3@illinois.edu>
2016-04-19 10:18:47 -0500

    html files for different routes created

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-import camera
-import face
+#import camera
+#import face
 from flask import Flask, render_template, request, url_for
 
 app = Flask(__name__)
@@ -18,6 +18,22 @@ among other things.
 @app.route('/')
 def home():
 	return render_template('home.html')
+	
+@app.route('/program', methods=['POST'])
+def program():
+	req=request.form['info']
+	if req == 'manip data':
+		# Some function to manipulate or view certain data from database
+		return render_template('do_something.html')
+	elif req == 'original pictures':
+		return render_template('view_pictures_original.html')
+	elif req == 'detected pictures':
+		return render_template('view_pictures_detected.html')
+	elif req == 'start camera':
+		# Some function here to start the camera
+		return render_template('camera_start.html')
+	else:
+		return render_template('do_something.html')
 
 if __name__ == '__main__':
 	app.run(debug=True)
