161325a1acd3893a7e87a345afb9e644f9dd5a26
Ben Chase <bbchase@illinois.edu>
2016-05-03 02:32:41 -0500

    cleanup

--- face.py
+++ face.py
@@ -5,7 +5,7 @@ import shutil
 # args: imgpath, output imgpath, cascPath
 #TODO scanned_originals
 def numfaces():
-    imagePath = "static/photos_orig/img3.jpg"
+    imagePath = "static/photos_orig/face1.jpg"
     #cascPath = sys.argv[2]
 
     faceCascade = cv2.CascadeClassifier('/usr/local/Cellar/opencv/2.4.12_2/share/OpenCV/haarcascades/haarcascade_frontalface_default.xml')
