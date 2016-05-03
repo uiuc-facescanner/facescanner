3314913891086516fa534ea5211f5327bf7d40ba
Ben Chase <bbchase@illinois.edu>
2016-05-03 17:34:56 -0500

    Removed unused db code

--- app.py
+++ app.py
@@ -56,50 +56,6 @@ def view_original():
 		files=glob.glob(path)
 		return render_template('view-pictures-original.html', files=files)
 
-
-# face database
-# Load default config and override config from an environment variable
-app.config.update(dict(
-	DATABASE=os.path.join(app.root_path, 'face.db'),
-	DEBUG=True,
-	SECRET_KEY='development key',
-	USERNAME='admin',
-	PASSWORD='default'
-))
-app.config.from_envvar('RESTORAN_SETTINGS', silent=True)
-
-def connect_db():
-	rv = sqlite3.connect(app.config['DATABASE'])
-	rv.row_factory = sqlite3.Row
-	return rv
-
-@app.route('/new_db')
-def new_db():
-	db = get_db()
-	with app.open_resource('schema.sql', mode='r') as f:
-		db.cursor().executescript(f.read())
-	db.commit()
-	return render_template('new-db.html')
-
-@app.cli.command('initdb')
-def initdb_command():
-	init_db()
-	print('Initialized the database.')
-
-def get_db():
-	if not hasattr(g, 'sqlite_db'):
-		g.sqlite_db = connect_db()
-	return g.sqlite_db
-
-@app.teardown_appcontext
-def close_db(error):
-	if hasattr(g, 'sqlite_db'):
-		g.sqlite_db.close()
-
-
-# end face database
-
-
 if __name__ == '__main__':
 	# camera_init()
 	app.run(debug=True)
